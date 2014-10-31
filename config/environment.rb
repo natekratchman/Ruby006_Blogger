9174dd6fdb9f549a682888f8b1339e9c06fa3066
natekratchman <natecrashman@gmail.com>
2014-10-31 18:21:22 -0400

    [fully functional] pre-heroku publishing

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,29 @@ require 'date'
 require 'open-uri'
 require 'rss'
 
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
 require_all 'app'
 require_all 'lib'
+
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+
+
+configure :development do
+ ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite" 
+  )
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
